d1feb5a5c35e0123eeb300ec2516514290fb07e7
Richard Li <richardli125@gmail.com>
2019-08-30 18:57:19 -0400

    addded routes for login and going to admin or customer

--- src/Components/ProfilePage.js
+++ src/Components/ProfilePage.js
@@ -1,33 +1,69 @@
-import React, { Component } from 'react'
-import Manager from './Manager'
-import AdminOverviewContainer from '../Containers/AdminOverviewContainer'
+import React, { Component } from "react";
+import Manager from "./Manager";
+import AdminOverviewContainer from "../Containers/AdminOverviewContainer";
+import { Link } from "react-router-dom";
+import Login from './LoginPage'
 
 class ProfilePage extends Component {
-
   state = {
-    username: ''
-  }
+    currentUser: {}
+  };
 
   componentDidMount() {
-    fetch('http://localhost:3000/profile', {
-      headers: {
-        Authorization: localStorage.token
-      }
-    })
-    .then(res => res.json())
-    .then(profileInfo => this.setState({
-      username: profileInfo.first_name 
-    }))
+    this.checkForToken()
   }
 
+
+   checkForToken = () => {
+     if (localStorage.token) {
+      fetch("http://localhost:3000/profile", {
+        headers: {
+          Authorization: localStorage.token
+        }
+      })
+        .then(res => res.json())
+        .then(parsedData =>
+          this.setState({
+            currentUser: parsedData
+          })
+        );
+     } else {
+       this.props.history.push('/login')
+     }
+   }
+
   render() {
-    return (
-      <div>
-        <h1>Profile Page you're {this.state.username}</h1>
-        <AdminOverviewContainer />
-      </div>
-    )
+    const {
+      first_name,
+      last_name,
+      email,
+      phone_number,
+      address,
+      city,
+      state,
+      zipcode,
+      username,
+      admin
+    } = this.state.currentUser;
+
+    if (admin && localStorage.token) {
+      return <AdminOverviewContainer currentUser = 'currentUser' />;
+    } else if (localStorage.token) {
+      return (
+        <div>
+          <h1>Profile Page you're {`${first_name} ${last_name}`}</h1>
+          <h3>Email: {email}</h3>
+          <h3>Phone Number: {phone_number}</h3>
+          <h3>UserName: {username}</h3>
+          <h3>City: {city}</h3>
+          <h3>state: {state}</h3>
+          {admin ? <Link to='/admin'>Manage</Link> : null}
+        </div>
+      );
+    } else {
+      return <Login />
+    }
   }
 }
 
-export default ProfilePage
+export default ProfilePage;
