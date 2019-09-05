ef995638722c7e651379a4ef369e3c6b26a5d914
Richard Li <richardli125@gmail.com>
2019-09-05 19:34:41 -0400

    Updated

--- src/Containers/ProductFormContainer.js
+++ src/Containers/ProductFormContainer.js
@@ -2,7 +2,55 @@ import React, { Component } from 'react'
 import { faPiggyBank } from '@fortawesome/free-solid-svg-icons';
 
 class ProductFormContainer extends Component {
+  state = {
+    name: '',
+    color: '',
+    size: '',
+    quantity: '',
+    price: '',
+    varientId: '',
+    imageUrl: '',
+    description: ''
+  }
+
+
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    fetch('http://localhost:3000/products', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        Authorization: localStorage.token
+      },
+      body: JSON.stringify({
+        ...this.state
+      })
+    })
+    .then(resp => resp.json())
+    .then(parsedData => {
+      this.props.updateQuantity(parsedData)
+    })
+    this.props.handleAddClick()
+  }
+
+  // updateQuantity={this.props.updateQuantity}
+// })
+//   .then(resp => resp.json())
+//   .then(this.props.history.push("/login"));
+// };
+
+
+
   render() {
+    const { name, color, size, quantity, price, varientId, imageUrl, description} = this.state
     return (
       <div className="product-form-container">
         <header>
@@ -10,23 +58,23 @@ class ProductFormContainer extends Component {
           <button onClick={this.props.handleAddClick}>X</button>
         </header>
         <div className="product-form-wrapper">
-          <form>
+          <form onSubmit={this.handleSubmit}>
             <label htmlFor="name">Product Name:</label><br></br>
-            <input type="text" id="name"/><br></br>
+            <input onChange={this.handleChange} value={name} type="text" id="name" name="name"/><br></br>
             <label htmlFor="color">Color:</label><br></br>
-            <input type="text" id="color"/><br></br>
+            <input onChange={this.handleChange} value={color} type="text" id="color" name="color"/><br></br>
             <label htmlFor="size">Size:</label><br></br>
-            <input type="text" id="size"/><br></br>
+            <input onChange={this.handleChange} value={size} type="text" id="size" name="size"/><br></br>
             <label htmlFor="quantity">Quantity:</label><br></br>
-            <input type="number" id="quantity"/><br></br>
+            <input onChange={this.handleChange} value={quantity} type="number" id="quantity" name="quantity"/><br></br>
             <label htmlFor="price">Price:</label><br></br>
-            <input type="text" id="price"/><br></br>
+            <input onChange={this.handleChange} value={price} type="text" id="price" name="price"/><br></br>
             <label htmlFor="varientID">VarientID:</label><br></br>
-            <input type="text" id="varientID"/><br></br>
+            <input onChange={this.handleChange} value={varientId} type="text" id="varientID" name="varientId"/><br></br>
             <label htmlFor="image">ImageURL:</label><br></br>
-            <input type="text" id="image"/><br></br>
+            <input onChange={this.handleChange} value={imageUrl} type="text" id="image" name="imageUrl" /><br></br>
             <label htmlFor="description">Description:</label><br></br>
-            <textarea type="text" id="description"></textarea><br></br>
+            <textarea onChange={this.handleChange} value={description} type="text" id="description" name="description"></textarea><br></br>
             <input id="product-form-submit-btn" type="submit"/>
           </form>
         </div>
