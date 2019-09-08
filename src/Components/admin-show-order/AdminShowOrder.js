8db871825cf2d2b3b489d436dc276d6ebf74fafd
Richard Li <richardli125@gmail.com>
2019-09-07 20:04:48 -0400

    added show order info

---
+++ src/Components/admin-show-order/AdminShowOrder.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class AdminShowOrder extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default AdminShowOrder
