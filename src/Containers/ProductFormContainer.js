d62267cbf5b8e59ff703ac9c0271c7c385ad6095
Richard Li <richardli125@gmail.com>
2019-09-08 04:44:17 -0400

    updated product form

--- src/Containers/ProductFormContainer.js
+++ src/Containers/ProductFormContainer.js
@@ -1,14 +1,26 @@
-import React, { Component } from 'react'
-import ProductForm from '../Components/ProductForm'
+import React, { Component } from "react";
+import ProductForm from "../Components/ProductForm";
 
-const ProductFormContainer = ({title, handleAddClick, product, updateQuantity}) => (
-  <div className="product-form-container">
-        <header>
-          <h2>{title}</h2>
-          <button onClick={handleAddClick}>X</button>
-        </header>
-        <ProductForm product={product} updateQuantity={updateQuantity} handleAddClick={handleAddClick}/>
-      </div>
-)
+const ProductFormContainer = ({
+  title,
+  handleAddClick,
+  product,
+  updateQuantity,
+  ...otherProps
+}) => (
+  <div className='product-form-container'>
+    <header>
+      <h2>{title}</h2>
+      <button onClick={handleAddClick}>X</button>
+    </header>
+    {title === "Add a Product" || title === "Edit a Product" ? (
+      <ProductForm
+        product={product}
+        updateQuantity={updateQuantity}
+        handleAddClick={handleAddClick}
+      />
+    ) : null}
+  </div>
+);
 
-export default ProductFormContainer
+export default ProductFormContainer;
