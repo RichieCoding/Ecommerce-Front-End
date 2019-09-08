5df6377e93d6f478a3ee92fdcd884bae2171e8e5
Richard Li <richardli125@gmail.com>
2019-09-07 20:03:11 -0400

    updated admin info box

--- src/Components/admin-info-box/AdminInfoBox.js
+++ src/Components/admin-info-box/AdminInfoBox.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./admin-info-box.styles.scss";
+import CountUp from 'react-countup'
 
-const AdminInfoBox = ({ title, info }) => (
+const AdminInfoBox = ({ title, info, price, customer }) => (
   <div className='admin-info-box'>
     <header><h3 className="title">{title}</h3></header>
-    {info ? <div><h2>{info}</h2></div> : null}
+    {info ? <div className='info'><h2><CountUp end={info} /></h2></div> : null}
+    {price ? <div className='price'><h2>$ <CountUp duration={1} end={price} /></h2></div> : null}
+    {customer ? <div className='customer'><h2>{customer}</h2></div> : null}
   </div>
 );
 
