25a957872c9a5378fc59d5c39891b962afabce50
Richard Li <richardli125@gmail.com>
2019-09-09 03:11:47 -0400

    added single order preview for customer profile page

---
+++ src/Components/customer-single-order-preview/CustomerSingleOrderPreview.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import "./customer-single-order-preview.styles.scss";
+import AdminShowOrder from "../admin-show-order/AdminShowOrder";
+
+class CustomerSingleOrderPreview extends Component {
+  state = {
+    clicked: false,
+    currentOrderDetails: []
+  };
+
+  handleClick = () => {
+    console.log(this.props.orderId);
+    fetch(`http://localhost:3000/orders/${this.props.orderId}`, {
+      headers: {
+        Authorization: localStorage.token
+      }
+    })
+      .then(resp => resp.json())
+      .then(orderDetails =>
+        this.setState({
+          currentOrderDetails: orderDetails,
+          clicked: !this.state.clicked
+        })
+      );
+  };
+
+  handleOrderModal = () => {
+    this.setState({
+      clicked: !this.state.clicked
+    });
+  };
+
+  render() {
+    const { orderNumber } = this.props;
+    return (
+      <>
+        <div className='customer-single-order-preview'>
+          <h3>{`Order #${orderNumber + 1}`}</h3>
+          <button onClick={this.handleClick}>View</button>
+        </div>
+        {this.state.clicked ? (
+          <AdminShowOrder
+            customer={true}
+            orderDetails={this.state.currentOrderDetails}
+            handleOrderModal={this.handleOrderModal}
+          />
+        ) : null}
+      </>
+    );
+  }
+}
+
+export default CustomerSingleOrderPreview;
