595207b129ce3a36f57d778d0a37f670e0100f18
Richard Li <richardli125@gmail.com>
2019-09-09 03:10:59 -0400

    added headers to shop home page

--- src/Containers/ShopHomePage.js
+++ src/Containers/ShopHomePage.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import Header from '../Components/header/Header'
 
 class ShopHomePage extends Component {
   render() {
     return (
+      <>
+      <Header />
       <div>
         <h1>HomePage SHOP</h1>
         <Link to='/login'>Login</Link>
       </div>
+      </>
     )
   }
 }
