1de5ff6d6bf873203b3862ed27c60e959eb100e5
Richard Li <richardli125@gmail.com>
2019-09-10 00:31:11 -0400

    added headers to all pages

--- src/App.js
+++ src/App.js
@@ -5,12 +5,21 @@ import ProfilePage from "./Components/ProfilePage";
 import SignUpPage from "./Containers/SignUpContainer";
 import AdminOverviewContainer from "./Containers/AdminOverviewContainer";
 import ShopHomePage from "./Containers/ShopHomePage";
+import Header from "./Components/header/Header"
+import CartPage from "./Containers/cart-page/CartPage"
 import { Switch, Route } from "react-router-dom";
 
 class App extends Component {
   state = {
     page: "login",
-    products: []
+    products: [],
+    currentUser: {
+      admin: false
+    },
+    cart: {
+      products: []
+    }
+     
   };
 
   redirect = page => {
@@ -20,7 +29,39 @@ class App extends Component {
   };
 
   componentDidMount() {
-    this.fetchProducts()
+    this.fetchProducts();
+    fetch('http://localhost:3000/profile', {
+      headers: {
+        'Authorization': localStorage.token
+      }
+    })
+    .then(resp => resp.json())
+    .then(parsedData => {
+      if (localStorage.token) {
+        this.fetchCartItems(parsedData.cart.id)
+      
+      this.setState({
+        currentUser: parsedData
+      })
+    }
+    })
+  }
+
+  handleCartFetch = () => {
+    this.componentDidMount()
+  }
+
+  // Fetch Cart Items
+  fetchCartItems = (id) => {
+    fetch(`http://localhost:3000/carts/${id}`, {
+      headers: {
+        'Authorization': localStorage.token
+      }
+    })
+    .then(resp => resp.json())
+    .then(cartData => this.setState({
+      cart: cartData
+    }))
   }
 
   // Fetch all products
@@ -35,17 +76,33 @@ class App extends Component {
           products: parsedData
         });
       });
-  }
+  };
+
+  // Get Current User from LoginPage
+  // getCurrentUser = userData => {
+  //   console.log(`hello ${userData}`);
+  //   this.setState({
+  //     currentUser: userData
+  //   })
+  // };
 
   updateQuantity = data => {
-    this.fetchProducts()
+    this.fetchProducts();
   };
+
 
   render() {
     return (
+      <>
+      <Header cart={this.state.cart} handleCartFetch={this.handleCartFetch} currentUser={this.state.currentUser} />
       <Switch>
-        <Route exact path='/' component={ShopHomePage} />
-        <Route path='/login' component={LoginPage} />
+        <Route exact path='/' render={routerProps => (<ShopHomePage cart={this.state.cart} />)} />
+        <Route
+          path='/login'
+          render={routerProps => (
+            <LoginPage handleCartFetch={this.handleCartFetch} getCurrentUser={this.getCurrentUser} {...routerProps} />
+          )}
+        />
         <Route
           path='/profile'
           render={routerProps => (
@@ -53,11 +110,23 @@ class App extends Component {
               {...routerProps}
               products={this.state.products}
               updateQuantity={this.updateQuantity}
+              currentUser={this.state.currentUser}
+              cart={this.state.cart}
+            />
+          )}
+        />
+        <Route
+          path='/cart'
+          render={routerProps => (
+            <CartPage
+              {...routerProps}
+              currentUser={this.state.currentUser}
             />
           )}
         />
         <Route path='/signup' component={SignUpPage} />
       </Switch>
+      </>
     );
   }
 }
