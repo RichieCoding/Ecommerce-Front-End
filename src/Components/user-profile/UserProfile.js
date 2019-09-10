5796de9b2c3fa63588e31752401cb8d0dbbe99bc
Richard Li <richardli125@gmail.com>
2019-09-10 00:32:51 -0400

    updated

--- src/Components/user-profile/UserProfile.js
+++ src/Components/user-profile/UserProfile.js
@@ -2,50 +2,59 @@ import React from "react";
 import "./user-profile.styles.scss";
 import CustomerProfileForm from "../customer-profile-form/CustomerProfileForm";
 import CustomerSingleOrderPreview from "../customer-single-order-preview/CustomerSingleOrderPreview";
-import Header from '../header/Header'
+import Header from "../header/Header";
 
 const UserProfile = props => {
   const { first_name, username } = props.userInfo;
-  const renderOrders = props.usersOrders.map((order, index) => {
-    return (
-      <CustomerSingleOrderPreview
-        key={order.id}
-        orderNumber={index}
-        userId={order.user_id}
-        orderId={order.id}
-      />
-    );
-  });
+  const renderOrders =
+    props.usersOrders !== undefined
+      ? props.usersOrders.map((order, index) => {
+          return (
+            <CustomerSingleOrderPreview
+              key={order.id}
+              orderNumber={index}
+              userId={order.user_id}
+              orderId={order.id}
+            />
+          );
+        })
+      : null;
   // const renderOrders = orders.map()
   return (
     <>
-      <Header />
-    <div className='user-profile'>
-      <div className='user-container'>
-        <header className="welcome">
-          <h2>{ first_name !== null ? `Welcome ${first_name}` : `Welcome ${username}`}</h2>
-        </header>
-        <main>
-          {/* All Order Details */}
-          <div className='order-details'>
-            <div className='all-order-title'>
-              <h3>All Orders</h3>
+      {/* <Header cart={props.cart} /> */}
+      <div className='user-profile'>
+        <div className='user-container'>
+          <header className='welcome'>
+            <h2>
+              {first_name !== null
+                ? `Welcome ${first_name}`
+                : `Welcome ${username}`}
+            </h2>
+          </header>
+          <main>
+            {/* All Order Details */}
+            <div className='order-details'>
+              <div className='all-order-title'>
+                <h3>All Orders</h3>
+              </div>
+              <div>{renderOrders}</div>
             </div>
-            <div>
-              {renderOrders}
-            </div>
-          </div>
-          {/* All User Info */}
-          <div className='all-user-info'>
-            <div className='edit-title'>
-              <h2>Your Profile</h2>
-            </div>
+            {/* All User Info */}
+            <div className='all-user-info'>
+              <div className='edit-title'>
+                <h2>Your Profile</h2>
+              </div>
 
-            <CustomerProfileForm usersOrders={props.usersOrders} userInfo={props.userInfo}/>
-          </div>
-        </main>
+              <CustomerProfileForm
+                currentUser={props.currentUser}
+                usersOrders={props.usersOrders}
+                userInfo={props.userInfo}
+              />
+            </div>
+          </main>
+        </div>
       </div>
-    </div>
     </>
   );
 };
