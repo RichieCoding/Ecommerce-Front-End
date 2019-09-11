78f0187bc252037475201304eff2e6e497774c2e
Richard Li <richardli125@gmail.com>
2019-09-10 22:21:14 -0400

    updated

--- src/Containers/ShopHomePage.js
+++ src/Containers/ShopHomePage.js
@@ -1,16 +1,13 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+
 import Header from '../Components/header/Header'
 
 class ShopHomePage extends Component {
   render() {
     return (
-      <>
-      {/* <Header cart={this.props.cart} /> */}
       <div>
         
       </div>
-      </>
     )
   }
 }
