ad10fb72e46cc5ae19d23bcbdec72869d4581e53
Richard Li <richardli125@gmail.com>
2019-09-10 22:22:44 -0400

    added shop product page

---
+++ src/Containers/shop-product-page/ShopProductPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './shop-product-page.styles.scss'
+
+class ShopProductPage extends Component {
+  render() {
+    return (
+      <div>
+        <p>shop</p>
+      </div>
+    )
+  }
+}
+
+export default ShopProductPage
