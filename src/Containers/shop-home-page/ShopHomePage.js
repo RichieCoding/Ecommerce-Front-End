9d8aef42f088ce3324aade1bd5b537dcc19baaa7
Richard Li <richardli125@gmail.com>
2019-10-20 16:25:19 -0400

    changed localhost to heroku link

--- src/Containers/shop-home-page/ShopHomePage.js
+++ src/Containers/shop-home-page/ShopHomePage.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import './shop-home-page.styles.scss'
+import './shop-home-page.styles.scss';
+import Shoppie from '../../assets/images/ShoppieBanner.png';
+import { Link } from 'react-router-dom';
 
 
 const ShopHomePage = () => (
   <div className='shop-home-page'>
     <div className='banner'>
-      
+      <img src={Shoppie} alt="banner"/>
+    </div>
+    <div className="secondary-banner">
+      <Link to='/shop'><div className="first small-banner"><h3>SHOP</h3></div></Link>
+      <Link to='/shop'><div className="second small-banner"><h3>SALE</h3></div></Link>
     </div>
   </div>
 )
