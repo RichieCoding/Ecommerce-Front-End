390a00b1bb61c7b0913c9df5de9b62a6808df378
Richard Li <richardli125@gmail.com>
2019-10-23 22:05:43 -0400

    removed second banner

--- src/Containers/shop-home-page/ShopHomePage.js
+++ src/Containers/shop-home-page/ShopHomePage.js
@@ -9,10 +9,6 @@ const ShopHomePage = () => (
     <div className='banner'>
       <img src={Shoppie} alt="banner"/>
     </div>
-    <div className="secondary-banner">
-      <Link to='/shop'><div className="first small-banner"><h3>SHOP</h3></div></Link>
-      <Link to='/profile'><div className="second small-banner"><h3>PROFILE</h3></div></Link>
-    </div>
   </div>
 )
 
