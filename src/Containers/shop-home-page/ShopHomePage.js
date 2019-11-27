4811f4a1ad27db3baa527f65d16b0f0c44d446bf
Richard Li <richardli125@gmail.com>
2019-11-27 16:42:38 -0500

    added featured products

--- src/Containers/shop-home-page/ShopHomePage.js
+++ src/Containers/shop-home-page/ShopHomePage.js
@@ -2,13 +2,27 @@ import React from "react";
 import "./shop-home-page.styles.scss";
 // import { Link } from "react-router-dom";
 import { Banner } from "../../Components/banner/Banner";
+import ProductCard from "../../Components/product-card/ProductCard";
 
-const ShopHomePage = () => (
+const ShopHomePage = props => (
   <div className='shop-home-page'>
     <Banner title='New Arrivals' button='SHOP' />
-    {/* <div className='featured-items'>
+    <div className='featured-items'>
       <h3>Featured Items</h3>
-    </div> */}
+      <div className='products'>
+        {props.featured.map(product => {
+          return (
+            <ProductCard
+              key={product.id}
+              product={product}
+              cartId={props.cartId}
+              handleCartFetch={props.handleCartFetch}
+              cart={props.cart}
+            />
+          );
+        })}
+      </div>
+    </div>
   </div>
 );
 
