f9043a4b6bf9ea7faae25241737e60bd752a3704
Richard Li <richardli125@gmail.com>
2019-12-03 04:29:56 -0500

    refactored to use useState and useEffect

--- src/Containers/single-product-page/SingleProductPage.js
+++ src/Containers/single-product-page/SingleProductPage.js
@@ -1,51 +1,50 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import "./single-product-page.styles.scss";
 import { Link } from "react-router-dom";
 import Spinner from "../../Components/Spinner/Spinner";
-import URL from '../../Components/URL'
+import URL from "../../Components/URL";
 
-class SingleProductPage extends Component {
-  state = {
-    product: {},
-    sizeLetter: "",
-    status: "Add to cart",
-    loading: true
-  };
+const SingleProductPage = ({ match, cart, cartId, handleCartFetch }) => {
+  const [product, setProduct] = useState({});
+  const [sizeLetter, setSizeLetter] = useState("");
+  const [status, setStatus] = useState("Add to cart");
+  const [loading, setLoading] = useState(false);
 
-  componentDidMount() {
-    const productId = this.props.match.params.id;
+  useEffect(() => {
+    setLoading(true);
+
+    const productId = match.params.id;
+
     fetch(`${URL}/products/${productId}`)
       .then(res => res.json())
-      .then(parsedData => {
-        console.log(parsedData);
-        this.setState({
-          product: parsedData,
-          sizeLetter: parsedData.size.charAt(0),
-          loading: false
-        });
-        const { quantity } = parsedData;
-        if (quantity === 0) {
-          this.setState({
-            status: "SOLD OUT"
-          });
-        } else if (this.props.cart.length !== 0) {
-          for (let i = 0; i < this.props.cart.length; i++) {
-            if (this.props.cart[i].product_id === this.state.product.id) {
-              this.setState({
-                status: "Added"
-              });
-            }
-          }
-        }
+      .then(parsedProduct => {
+        setProduct(parsedProduct);
+        setSizeLetter(parsedProduct.size.charAt(0));
+        setLoading(false);
+        const { quantity } = parsedProduct;
+
+        defineStatus(quantity);
       });
+  }, []);
+
+  // Defines what the status of product dependent on quantity
+  const defineStatus = (quantity) => {
+    if (quantity === 0) {
+      setStatus("SOLD OUT");
+    } else if (cart.length !== 0) {
+      for (let i = 0; i < cart.length; i++) {
+        if (cart[i].product_id === product.id) {
+          setStatus("Added");
+        }
+      }
+    }
   }
 
-  addToCart = () => {
-    const findProduct = this.props.cart.find(cartItem => {
-      return cartItem.product_id === this.state.product.id;
+  const addToCart = () => {
+    const findProduct = cart.find(cartItem => {
+      return cartItem.product_id === product.id;
     });
     if (!findProduct) {
-      console.log("not in cart");
       fetch(`${URL}/cart_items`, {
         method: "POST",
         headers: {
@@ -54,90 +53,75 @@ class SingleProductPage extends Component {
           Authorization: localStorage.token
         },
         body: JSON.stringify({
-          cart_id: this.props.cartId,
-          product_id: this.state.product.id
+          cart_id: cartId,
+          product_id: product.id
         })
       })
         .then(resp => resp.json())
         .then(res => {
-          this.setState({
-            status: "Added"
-          });
-          this.props.handleCartFetch();
+          setStatus("Added");
+          handleCartFetch();
         });
     }
   };
 
-  renderSizeLetter = () => {
-    console.log(this.state.sizeLetter);
-  };
+  if (loading) return <Spinner />;
+  const { name, color, imageUrl, price, description, size } = product;
+  return (
+    <>
+      <div className='back-btn-container'>
+        <Link to='/shop'>
+          <p className='back-btn'>Back to products</p>
+        </Link>
+      </div>
 
-  render() {
-    if (this.state.loading) return <Spinner />
-    const {
-      name,
-      color,
-      imageUrl,
-      price,
-      description,
-      size
-    } = this.state.product;
-    return (
-      <>
-        <div className='back-btn-container'>
-          <Link to='/shop'>
-            <p className='back-btn'>Back to products</p>
-          </Link>
+      <div className='single-product-page'>
+        <div className='product-container'>
+          <h3 className='product-name'>{name}</h3>
+          <h3 className='product-price'>{`$${price}.00`}</h3>
         </div>
 
-        <div className='single-product-page'>
-          <div className='product-container'>
-            <h3 className='product-name'>{name}</h3>
-            <h3 className='product-price'>{`$${price}.00`}</h3>
-          </div>
+        <div className='product-image'>
+          <img src={imageUrl} alt='product' />
+        </div>
 
-          <div className='product-image'>
-            <img src={imageUrl} alt='product' />
+        <div className='color-wrapper'>
+          <div className='color-container'>
+            <div
+              className='product-color'
+              style={{ background: `${color}` }}
+            ></div>
           </div>
+          <p className='color-text'>{color}</p>
+          <hr />
+        </div>
 
-          <div className='color-wrapper'>
-            <div className='color-container'>
-              <div
-                className='product-color'
-                style={{ background: `${color}` }}
-              ></div>
+        <div className='size-wrapper'>
+          <p className='size-text'>{size}</p>
+          <div className='product-size'>
+            <div className='size-container'>
+              <p className='size-letter'>{sizeLetter}</p>
             </div>
-            <p className='color-text'>{color}</p>
-            <hr />
-          </div>
-
-          <div className='size-wrapper'>
-            <p className='size-text'>{size}</p>
-            <div className='product-size'>
-              <div className='size-container'>
-                <p className='size-letter'>{this.state.sizeLetter}</p>
-              </div>
-            </div>
-          </div>
-
-          <div className='add-to-cart-btn'>
-            {this.state.status === "SOLD OUT" ? (
-              <button className='add-to-cart'>{this.state.status}</button>
-            ) : (
-              <button onClick={this.addToCart} className='add-to-cart'>
-                {this.state.status}
-              </button>
-            )}
-          </div>
-
-          <div className='product-description'>
-            <p className='detail-title'>Details</p>
-            <p className='detail-description'>{description}</p>
           </div>
         </div>
-      </>
-    );
-  }
-}
+
+        <div className='add-to-cart-btn'>
+          {status === "SOLD OUT" ? (
+            <button className='add-to-cart'>{status}</button>
+          ) : (
+            <button onClick={addToCart} className='add-to-cart'>
+              {status}
+            </button>
+          )}
+        </div>
+
+        <div className='product-description'>
+          <p className='detail-title'>Details</p>
+          <p className='detail-description'>{description}</p>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default SingleProductPage;
