a2fe40f02bfbd6eca29ad4f9d1e74ff46c9e8237
Richard Li <richardli125@gmail.com>
2019-12-06 05:36:38 -0500

    changed to functional component

--- src/Containers/shop-product-page/ShopProductPage.js
+++ src/Containers/shop-product-page/ShopProductPage.js
@@ -1,25 +1,22 @@
 import React, { Component } from "react";
 import "./shop-product-page.styles.scss";
 import ProductCard from "../../Components/product-card/ProductCard";
-import Spinner from '../../Components/Spinner/Spinner'
+import Spinner from "../../Components/Spinner/Spinner";
 
-class ShopProductPage extends Component {
-
-  render() {
-    const renderProductCards = this.props.products.map(product => {
-      return (
-        <ProductCard
-          cart={this.props.cart}
-          cartId={this.props.cartId}
-          product={product}
-          handleCartFetch={this.props.handleCartFetch}
-        />
-      );
-    });
-    const { productsLoaded } = this.props
-    if (!productsLoaded) return <Spinner />
-    return <div className='shop-product-page'>{renderProductCards}</div>;
-  }
-}
+const ShopProductPage = (props) => {
+  const renderProductCards = props.products.map(product => {
+    return (
+      <ProductCard
+        cart={props.cart}
+        cartId={props.cartId}
+        product={product}
+        handleCartFetch={props.handleCartFetch}
+      />
+    );
+  });
+  const { productsLoaded } = props;
+  if (!productsLoaded) return <Spinner />;
+  return <div className='shop-product-page'>{renderProductCards}</div>;
+};
 
 export default ShopProductPage;
