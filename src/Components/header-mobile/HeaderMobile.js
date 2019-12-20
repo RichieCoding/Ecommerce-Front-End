c116492554947e608d23f315be2a7e70e0102a28
Richard Li <richardli125@gmail.com>
2019-12-20 04:03:22 -0500

    changed to functional component

--- src/Components/header-mobile/HeaderMobile.js
+++ src/Components/header-mobile/HeaderMobile.js
@@ -1,42 +1,39 @@
-import React, { Component } from "react";
-import "./header-mobile.styles.scss";
-import { Link } from "react-router-dom";
+import React from 'react';
+import './header-mobile.styles.scss';
+import { Link } from 'react-router-dom';
 
-class HeaderMobile extends Component {
-
-  handleClick = () => {
+const HeaderMobile = ({ menuClicked }) => {
+  const handleClick = () => {
     this.props.menuClicked();
     localStorage.clear();
     this.props.handleCartFetch();
     this.props.setCartToZero();
   };
 
-  render() {
-    return (
-      <div className='header-mobile'>
-        <Link to='/' onClick={this.props.menuClicked}>
-          <h3>Home</h3>
+  return (
+    <div className='header-mobile'>
+      <Link to='/' onClick={menuClicked}>
+        <h3>Home</h3>
+      </Link>
+      <Link to='/shop' onClick={menuClicked}>
+        <h3>Shop</h3>
+      </Link>
+      {localStorage.token ? (
+        <Link to='/profile' onClick={menuClicked}>
+          <h3>Profile</h3>
         </Link>
-        <Link to='/shop' onClick={this.props.menuClicked}>
-          <h3>Shop</h3>
+      ) : (
+        <Link to='/login' onClick={menuClicked}>
+          <h3>Login</h3>
         </Link>
-        {localStorage.token ? (
-          <Link to='/profile' onClick={this.props.menuClicked}>
-            <h3>Profile</h3>
-          </Link>
-        ) : (
-          <Link to='/login' onClick={this.props.menuClicked}>
-            <h3>Login</h3>
-          </Link>
-        )}
-        {localStorage.token ? (
-          <Link to='/' onClick={this.handleClick}>
-            <h3>Log out</h3>
-          </Link>
-        ) : null}
-      </div>
-    );
-  }
-}
+      )}
+      {localStorage.token ? (
+        <Link to='/' onClick={handleClick}>
+          <h3>Log out</h3>
+        </Link>
+      ) : null}
+    </div>
+  );
+};
 
 export default HeaderMobile;
