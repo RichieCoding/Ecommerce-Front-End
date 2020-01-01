1f8d2f527c0803eac9a7d58d0f6da4ebda27aacb
Richard <richardli125@gmail.com>
2020-01-01 18:55:48 -0500

    added redux store and connected provider to entire app

--- src/index.js
+++ src/index.js
@@ -3,13 +3,18 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import ScrollToTop from './Components/ScrollToTop/ScrollToTop'
+import ScrollToTop from "./Components/ScrollToTop/ScrollToTop";
+import { Provider } from "react-redux";
+
+import store from './redux/store';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <ScrollToTop>
-      <App />
-    </ScrollToTop>
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <ScrollToTop>
+        <App />
+      </ScrollToTop>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
